ca2d5b6769726c123738abbc522a1d6196d69486
Dmitry M <s@hitagi.ru>
2015-06-23 17:30:19 +0400

    Обновил плагин "граватар" под текущую версию системы

--- common/plugins/gravatar/classes/modules/gravatar/entity/User.entity.class.php
+++ common/plugins/gravatar/classes/modules/gravatar/entity/User.entity.class.php
@@ -1,13 +1,40 @@
 <?php
 class PluginGravatar_ModuleGravatar_EntityUser extends PluginGravatar_Inherit_ModuleUser_EntityUser {
-  
-	//*********************************************************************************	  
-    public function getProfileAvatarPath($iSize=100) {
-        if($sPath=$this->getProfileAvatar()){ 	
-        	return str_replace('_100x100',(($iSize==0)?"":"_{$iSize}x{$iSize}"),$sPath."?".date('His',strtotime($this->getProfileDate())));
-    	}else{
-    		return "http://www.gravatar.com/avatar/".md5(strtolower($this->getMail())).".png?size=".$iSize."&d=identicon";
-    	}
+
+    /**
+     * Возвращает полный URL до аватары нужного размера
+     *
+     * @param int|string $xSize - Размер (120 | '120x100')
+     *
+     * @return  string
+     */
+    public function getAvatarUrl($xSize) {
+        if (!$xSize) {
+            $xSize = Config::Get('module.user.profile_avatar_size');
+            if (!$xSize) {
+                $xSize = self::DEFAULT_AVATAR_SIZE;
+            }
+        }
+
+        $sPropKey = '_avatar_url_' . $xSize;
+        $sUrl = $this->getProp($sPropKey);
+        if (is_null($sUrl)) {
+            if ($sRealSize = C::Get('module.uploader.images.profile_avatar.size.' . $xSize)) {
+                $xSize = $sRealSize;
+            }
+            $sUrl = $this->_getProfileImageUrl('profile_avatar', $xSize);
+            // Old version compatibility
+            $sUrl = $this->getProfileAvatar();
+            if ($sUrl) {
+                if ($xSize) {
+                    $sUrl = E::ModuleUploader()->ResizeTargetImage($sUrl, $xSize);
+                }
+            } else {
+                $sUrl = "http://www.gravatar.com/avatar/".md5(strtolower($this->getMail())).".png?size=".$xSize."&d=identicon";
+            }
+            $this->setProp($sPropKey, $sUrl);
+        }
+        return $sUrl;
     }
 }
-?>
+
