c13863af15b33410be08df75842ac5d0a65fc89a
Dmitry M <s@hitagi.ru>
2015-07-10 17:07:47 +0400

    Fix [betterspoilers]

--- common/plugins/betterspoilers/templates/skin/default/assets/js/betterspoilers.js
+++ common/plugins/betterspoilers/templates/skin/default/assets/js/betterspoilers.js
@@ -2,7 +2,7 @@
     $(document).ready(function () {
         // сраный костыль для сраного iCheck, который навязан всем чекбоксам без разбора
         setTimeout(function () {
-            if ($.iCheck) {
+            if ($.fn.iCheck) {
                 $('.betterspoiler > [class^="icheckbox_"] input, .spoiler > [class^="icheckbox_"] input').iCheck('destroy');
             }
         }, 1000);
