477239bec4a4bacb7a26f2b6f8cb1e6abde985d0
Dmitry M <s@hitagi.ru>
2015-07-10 16:07:42 +0400

    [customvideos] даём ссылку на Secured версию ютуб-плеера

--- common/plugins/customvideos/templates/skin/default/assets/js/youtube.js
+++ common/plugins/customvideos/templates/skin/default/assets/js/youtube.js
@@ -2,7 +2,7 @@ $(function () {
     // Подгружающийся по клику плеер youtube
     $(document).on('click', '[data-youtube-id]', function () {
         var self = $(this);
-        self.prev().attr('src', "http://www.youtube.com/embed/"+self.data('youtube-id')+"?wmode=opaque&theme=light&autoplay=1");
+        self.prev().attr('src', "https://www.youtube.com/embed/"+self.data('youtube-id')+"?wmode=opaque&theme=light&autoplay=1");
         self.remove();
     });
 });
